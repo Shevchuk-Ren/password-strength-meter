b1cd86dc1a5df57c1146fdb7c7cc9c957e79af45
victorianimchenko <77270393+Shevchuk-Ren@users.noreply.github.com>
2023-10-25 13:50:59 +0100

    clean space

--- src/app/password-strength-checklist/password-strength-checklist.component.ts
+++ src/app/password-strength-checklist/password-strength-checklist.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-password-strength-checklist',
